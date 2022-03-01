d01d7d0903d35419836e58833fbedfc73bf97299
Anastasia Protsvetkina <asifyoucared@mail.ru>
2022-03-01 14:43:29 +0300

    Update create_configs.sh

--- create_configs.sh
+++ create_configs.sh
@@ -1,4 +1,12 @@
 cp /usr/local/share/xquest/deffiles/xmm.def . 
 cp /usr/local/share/xquest/deffiles/xquest.def .
-cp /usr/local/share/xquest/deffiles/xproph.def .
 cp /usr/local/share/xquest/deffiles/Snakefile .
+
+case ":$PATH:" in
+  *:/usr/local/share/xquest/V2.1.5/xquest/bin:*) printf "PATH correctly set.\n\n"
+                                      ;;
+  *)  printf "Setting PATH... "
+      export PATH=$PATH:/usr/local/share/xquest/V2.1.5/xquest/bin
+      ;;
+esac
+xprophet.pl
